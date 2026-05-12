8bd3034ba3237874a7b60acc3dc87e82c2a9021d
ei2421 <ei2421@hamako-ths.ed.jp>
2026-05-12 12:23:34 +0900

    仮想環境

---
+++ app/database.py
@@ -0,0 +1,15 @@
+# SQLite接続
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}  # SQLite用
+)
+
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
